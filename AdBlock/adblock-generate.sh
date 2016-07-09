9e45dc3dd97c81e8e7dfa3e8538ba3c8ec36448f
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-07-09 19:25:35 +0200

    Update adblock-generate.sh

--- AdBlock/adblock-generate.sh
+++ AdBlock/adblock-generate.sh
@@ -17,15 +17,18 @@ TMP_LIST="/tmp/adblock.list"
 IP_ADDR=$(ifconfig  | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | cut -d' ' -f1 | head -n 1)
 
 # retrieve public lists
+echo "Retrieving public lists"
 wget -q -O - http://www.mvps.org/winhelp2002/hosts.txt | grep "0.0.0.0" | sed "s/0.0.0.0[ ]*//" | sed "s/.$//" > "$TMP_LIST"
 wget -q -O - http://adaway.org/hosts.txt | grep "127.0.0.1" | sed "s/127.0.0.1[ ]*//" >> "$TMP_LIST"
 wget -q -O - http://www.malwaredomainlist.com/hostslist/hosts.txt | grep "127.0.0.1" | sed "s/127.0.0.1[ ]*//" | sed "s/.$//" >> "$TMP_LIST"
 wget -q -O - --no-check-certificate https://hosts-file.net/ad_servers.txt | grep "127.0.0.1" | sed "s/127.0.0.1\t//" | sed "s/.$//" >> "$TMP_LIST"
 
 # retrieve private list
+echo "Retrieving private list"
 wget -q -O - --no-check-certificate "$URL_LIST" >> "$TMP_LIST"
 
 # concatenate, sort, deduplicate and format final list that is used by DNSMasq
+echo "Generating final adblock list"
 grep -v "#" "$TMP_LIST" | grep -v "^localhost" | grep -v "^127.0.0.1"| sort | uniq | sed "s/\(.*\)$/"$IP_ADDR"\t\1/" > "$FINAL_LIST"
 
 # display result
