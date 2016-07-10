f7a10114dc3eba1da13384ea78132d2407637526
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-07-10 20:03:09 +0200

    Update adaway-install.sh

--- AdAway/adaway-install.sh
+++ AdAway/adaway-install.sh
@@ -6,10 +6,23 @@
 #
 # 2016/07/09 - Version 1.0
 
-# update /etc/dnsmasq.conf
+# create and populate adaway site root
+mkdir /www-adaway
+wget -q -O /www-adaway/1x1.png --no-check-certificate "https://raw.githubusercontent.com/NicolasBernaerts/openwrt-scripts/master/AdAway/1x1.png"
+echo "<img width='1' height='1' src='/1x1.png' />" > "/www-adaway/index.html"
+
+# /etc/dnsmasq.conf
+# add adaway DNS host list
 echo "" >> /etc/dnsmasq.conf
 echo "# enable AdAway list" >> /etc/dnsmasq.conf
 echo "addn-hosts=/etc/adaway.hosts" >> /etc/dnsmasq.conf
+
+# /etc/config/uhttpd
+# change default ports to 1080 and 1443
+# add adaway site on ports 80 and 443
+cat /etc/config/uhttpd | sed "s/:80/:1080/g" | sed "s/:443/:1443/g" > /tmp/uhttpd.conf
+wget -q -O - --no-check-certificate "https://raw.githubusercontent.com/NicolasBernaerts/openwrt-scripts/master/AdAway/uhttpd.conf" >> /tmp/uhttpd.conf
+mv /tmp/uhttpd.conf /etc/config/uhttpd
 
 # install AdBlock list generation script
 wget -q -O /usr/bin/adaway-generate.sh --no-check-certificate "https://raw.githubusercontent.com/NicolasBernaerts/openwrt-scripts/master/AdAway/adaway-generate.sh"
@@ -17,11 +30,6 @@ chmod +x /usr/bin/adaway-generate.sh
 
 # generate AdBlock list
 /usr/bin/adaway-generate.sh
-
-# set LUCI 404 as 1x1 pixel
-wget -q -O /www/1x1.png --no-check-certificate "https://raw.githubusercontent.com/NicolasBernaerts/openwrt-scripts/master/AdAway/1x1.png"
-uci set uhttpd.main.error_page='/1x1.png'
-uci commit uhttpd
 
 # restart dnsmasq and uhttpd
 /etc/init.d/dnsmasq restart
