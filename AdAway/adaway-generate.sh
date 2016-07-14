03379b89926429fc9b5c91b2c7dbf139e3294140
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2016-07-14 16:00:33 +0200

    Update adaway-generate.sh

--- AdAway/adaway-generate.sh
+++ AdAway/adaway-generate.sh
@@ -4,37 +4,58 @@
 #
 # 2016/07/09 - Version 1.0
 
-# set your own private list URL
-URL_LIST="https://raw.githubusercontent.com/NicolasBernaerts/openwrt-scripts/master/AdAway/adaway-blacklist.list"
-
 # destination file
-FINAL_LIST="/etc/adaway.hosts"
+ADAWAY_DOMAIN="/etc/adaway-domain.conf"
+ADAWAY_BLACKLIST="/etc/adaway-blacklist.hosts"
 
 # set temporary file
-TMP_LIST="/tmp/adaway.list"
+TMP_FILE=$(mktemp)
 
 # get current IP adress
 IP_ADDR=$(ifconfig  | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | cut -d' ' -f1 | head -n 1)
 
-# retrieve public lists
-echo "Retrieving public lists"
-wget -q -O - "http://adaway.org/hosts.txt" | grep -v "^#" | sed "s/^[0-9 \.\t]*//" | cut -d' ' -f1 > "$TMP_LIST"
-wget -q -O - "http://winhelp2002.mvps.org/hosts.txt" | grep -v "^#" | sed "s/^[0-9 \.\t]*//" | cut -d' ' -f1 > "$TMP_LIST"
-wget -q -O - --no-check-certificate "https://hosts-file.net/ad_servers.txt" | grep -v "^#" | sed "s/^[0-9 \.\t]*//" | cut -d' ' -f1  >> "$TMP_LIST"
-wget -q -O - --no-check-certificate "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" | grep -v "^#" | sed "s/^[0-9 \.\t]*//" | cut -d' ' -f1  >> "$TMP_LIST"
+# --------------
+#  DOMAIN LISTS 
+# --------------
 
-# retrieve private list
-echo "Retrieving private list"
-wget -q -O - --no-check-certificate "$URL_LIST" >> "$TMP_LIST"
+# fanboy easylist
+echo "Retrieving fanboy easylist domain list"
+wget -q -O - https://secure.fanboy.co.nz/easylist.txt | grep "^||" | grep -v "[/$\*=]" | sed "s/||\(.*\)^/\1/" > "${TMP_FILE}"
 
-# concatenate, sort, deduplicate and format final list that is used by DNSMasq
-echo "Generating final adblock list"
-sort "$TMP_LIST" | uniq | sed "s/\(.*\)$/"$IP_ADDR"\t\1/" > "$FINAL_LIST"
+# concatenate, sort, deduplicate and format final domain list that is used by DNSMasq
+echo "Generating final domains list"
+sort "${TMP_FILE}" uniq | sed "s/\(.*\)$/server=\/\1\/${IP_ADDR}/" > "${ADAWAY_DOMAIN}"
 
-# display result
-NBR_SITE=$(wc -l "$FINAL_LIST" | cut -d' ' -f1)
-echo "$NBR_SITE sites will now be blocked"
+# ------------
+#  HOST LISTS 
+# ------------
+
+# adaway list
+echo "Retrieving adaway host list"
+wget -q -O - https://adaway.org/hosts.txt | grep -v "^#"  | grep -v "localhost" | cut -d' ' -f2 > "${TMP_FILE}"
+
+# winhelp2002 list
+echo "Retrieving winhelp2002 host list"
+wget -q -O - "http://winhelp2002.mvps.org/hosts.txt" | grep -v "^#" | cut -d' ' -f2 >> "${TMP_FILE}"
+
+# hosts-file.net list
+echo "Retrieving hosts-file.net host list"
+wget -q -O - "https://hosts-file.net/ad_servers.txt" | sed "s/^[0-9\.]*[^0-9a-z]*\(.*\)$/\1/" >> "${TMP_FILE}"
+
+# concatenate, sort, deduplicate and format final hosts list that is used by DNSMasq
+echo "Generating final hosts list"
+sort "${TMP_FILE}" | uniq | sed "s/\(.*\)$/"${IP_ADDR}"\t\1/" > "${ADAWAY_BLACKLIST}"
+
+# --------
+#  RESULT 
+# --------
 
 # restart DNSMasq
 echo "Restarting DNSMasq"
 /etc/init.d/dnsmasq restart
+
+# display result
+NBR_DOMAIN=$(wc -l "${ADAWAY_DOMAIN}" | cut -d' ' -f1)
+echo "$NBR_DOMAIN hosts will now be blocked"
+NBR_HOST=$(wc -l "${ADAWAY_BLACKLIST}" | cut -d' ' -f1)
+echo "$NBR_HOST hosts will now be blocked"
